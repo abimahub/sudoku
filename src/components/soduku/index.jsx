e1504c0d6a49348c5a41dc5663b1907c9b263e7a
AbiMA <48912566+indigosaurus@users.noreply.github.com>
2024-03-11 19:08:11 +0000

    tidied board

--- src/components/soduku/index.jsx
+++ src/components/soduku/index.jsx
@@ -1,6 +1,22 @@
 import { React, useEffect, useState } from "react";
 import "./styles.css";
 
+
+// board array layout (pipes for visual clarity):
+// a0,a1,a2 | b0,b1,b2 | c0,c1,c2
+// a3,a4,a5 | b3,b4,b5 | c3,c4,c5
+// a6,a7,a8 | b6,b7,b8 | c6,c7,c8
+//
+// d0,d1,d2 | e0,e1,e2 | f0,f1,f2
+// d3,d4,d5 | e3,e4,e5 | f3,f4,f5
+// d6,d7,d8 | e6,e7,e8 | f6,f7,f8
+//
+// g0,g1,g2 | h0,h1,h2 | i0,i1,i2
+// g3,g4,g5 | h3,h4,h5 | i3,i4,i5
+// g6,g7,g8 | h6,h7,h8 | i6,i7,i8
+//
+// this will allow checking of each 3x3 AND each row and column.
+
 function Square({ value, onClick }) {
   return (
     <button onClick={onClick} className="square">
@@ -24,12 +40,14 @@ export default function Sudoku() {
   }
 
   return (
-    <><h1>Sudoku</h1>
+    <>
+    <h1>Sudoku</h1>
     <div className="sudoku-main">
       
       <div className="sudoku-board">
+      <div className="row">
         <div className="grid-container">
-         
+        
           <Square value={squares[0]} onClick={() => handleClick(0)} />
           <Square value={squares[1]} onClick={() => handleClick(1)} />
           <Square value={squares[2]} onClick={() => handleClick(2)} />
@@ -41,7 +59,8 @@ export default function Sudoku() {
           <Square value={squares[0]} onClick={() => handleClick(0)} />
           <Square value={squares[1]} onClick={() => handleClick(1)} />
           <Square value={squares[2]} onClick={() => handleClick(2)} />
-         
+          </div>
+          <div className="grid-container">
           <Square value={squares[3]} onClick={() => handleClick(3)} />
           <Square value={squares[4]} onClick={() => handleClick(4)} />
           <Square value={squares[5]} onClick={() => handleClick(5)} />
@@ -53,6 +72,8 @@ export default function Sudoku() {
           <Square value={squares[3]} onClick={() => handleClick(3)} />
           <Square value={squares[4]} onClick={() => handleClick(4)} />
           <Square value={squares[5]} onClick={() => handleClick(5)} />
+          </div>
+          <div className="grid-container">
           <Square value={squares[6]} onClick={() => handleClick(6)} />
           <Square value={squares[7]} onClick={() => handleClick(7)} />
           <Square value={squares[8]} onClick={() => handleClick(8)} />
@@ -64,7 +85,10 @@ export default function Sudoku() {
           <Square value={squares[6]} onClick={() => handleClick(6)} />
           <Square value={squares[7]} onClick={() => handleClick(7)} />
           <Square value={squares[8]} onClick={() => handleClick(8)} />
-
+          </div>
+          </div>
+          <div className="row">
+          <div className="grid-container">
           <Square value={squares[0]} onClick={() => handleClick(0)} />
           <Square value={squares[1]} onClick={() => handleClick(1)} />
           <Square value={squares[2]} onClick={() => handleClick(2)} />
@@ -76,6 +100,8 @@ export default function Sudoku() {
           <Square value={squares[0]} onClick={() => handleClick(0)} />
           <Square value={squares[1]} onClick={() => handleClick(1)} />
           <Square value={squares[2]} onClick={() => handleClick(2)} />
+         </div>
+         <div className="grid-container">
           <Square value={squares[3]} onClick={() => handleClick(3)} />
           <Square value={squares[4]} onClick={() => handleClick(4)} />
           <Square value={squares[5]} onClick={() => handleClick(5)} />
@@ -87,6 +113,8 @@ export default function Sudoku() {
           <Square value={squares[3]} onClick={() => handleClick(3)} />
           <Square value={squares[4]} onClick={() => handleClick(4)} />
           <Square value={squares[5]} onClick={() => handleClick(5)} />
+         </div>
+         <div className="grid-container">
           <Square value={squares[6]} onClick={() => handleClick(6)} />
           <Square value={squares[7]} onClick={() => handleClick(7)} />
           <Square value={squares[8]} onClick={() => handleClick(8)} />
@@ -98,7 +126,10 @@ export default function Sudoku() {
           <Square value={squares[6]} onClick={() => handleClick(6)} />
           <Square value={squares[7]} onClick={() => handleClick(7)} />
           <Square value={squares[8]} onClick={() => handleClick(8)} />
-
+          </div>
+          </div>
+          <div className="row">
+          <div className="grid-container">
           <Square value={squares[0]} onClick={() => handleClick(0)} />
           <Square value={squares[1]} onClick={() => handleClick(1)} />
           <Square value={squares[2]} onClick={() => handleClick(2)} />
@@ -110,6 +141,8 @@ export default function Sudoku() {
           <Square value={squares[0]} onClick={() => handleClick(0)} />
           <Square value={squares[1]} onClick={() => handleClick(1)} />
           <Square value={squares[2]} onClick={() => handleClick(2)} />
+          </div>
+          <div className="grid-container">
           <Square value={squares[3]} onClick={() => handleClick(3)} />
           <Square value={squares[4]} onClick={() => handleClick(4)} />
           <Square value={squares[5]} onClick={() => handleClick(5)} />
@@ -121,6 +154,8 @@ export default function Sudoku() {
           <Square value={squares[3]} onClick={() => handleClick(3)} />
           <Square value={squares[4]} onClick={() => handleClick(4)} />
           <Square value={squares[5]} onClick={() => handleClick(5)} />
+          </div>
+          <div className="grid-container">
           <Square value={squares[6]} onClick={() => handleClick(6)} />
           <Square value={squares[7]} onClick={() => handleClick(7)} />
           <Square value={squares[8]} onClick={() => handleClick(8)} />
@@ -132,8 +167,10 @@ export default function Sudoku() {
           <Square value={squares[6]} onClick={() => handleClick(6)} />
           <Square value={squares[7]} onClick={() => handleClick(7)} />
           <Square value={squares[8]} onClick={() => handleClick(8)} />
-        </div>
-      </div>
+          </div>
+          </div>
+          </div>
+      
       <h3>
         {status}
       </h3>
